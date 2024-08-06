cd0056771a033fa39d0d691a2a43c0e37f6fc684
Secri <44509048+Secri@users.noreply.github.com>
2024-08-06 15:31:09 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,13 @@
-
+<?php
+      
+      function loadGtagEvents() {
+		
+		    wp_enqueue_script( 'add_gtag_event', get_stylesheet_directory_uri() . '/js/gtag_tracking.js' ); //On va chercher le script js
+	
+	    }
+	
+      if ( is_user_logged_in() ) { //Si l'internaute n'est pas connecté à l'interface d'administration
+		
+	      add_action( 'wp_enqueue_scripts', 'loadGtagEvents', 99); //On hook wp_enqueue_scripts et on met une priorité basse
+		
+      }
